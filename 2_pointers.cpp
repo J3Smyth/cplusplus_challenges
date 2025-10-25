4a08f69480c67584322c2ed13e87bfea77df93e7
Jude <judesmyth03@gmail.com>
2025-10-26 00:30:51 +0100

    Working on pointers

--- 2_pointers.cpp
+++ 2_pointers.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <iterator>
 using namespace std;
 
-int main(){
+// POINTERS point to a variable's memory address,
+// thus you can increment a ptr to iterate through an array
+int ptrPrint();
+int ptrIteration();
+int iMaxArrayElement(int *pi, int size);
+int iReverseArrayElement(int *pi, int size);
 
+int main()
+{
+    // ptrPrint();
+    ptrIteration();
+    // iMaxArrayElement(int *pi, int 10);
+    //*piReverseArray(int *pi, int 10);
+}
+
+int ptrPrint()
+{
     int bookID = 555;
     int *ptr = &bookID;
 
@@ -14,10 +30,29 @@ int main(){
 
     *ptr = 550;
 
-    cout << *ptr << endl << bookID << endl;
+    cout << *ptr << endl
+         << bookID << endl;
+    return 0;
+}
 
+int ptrIteration()
+{
+    const int SIZE = 6;
+    int idList[SIZE] = {10, 12, 14, 16, 18, 20};
+    cout << "Pointer Iteration\n--------------------\n";
+    for (int *i = idList; *i < SIZE; i++)
+    {
+        cout << "__ " << *i << " __" << endl;
+    }
+    return 0;
+}
 
+int iMaxArrayElement(int *pi, int size)
+{
+    return 0;
+}
 
-
-
+int *piReverseArray(int *pi, int size)
+{
+    return 0;
 }
