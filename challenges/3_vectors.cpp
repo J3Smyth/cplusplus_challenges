d194ff2542e4d900f5258f1c886460b4f8f52052
Jude <judesmyth03@gmail.com>
2025-10-26 23:56:19 +0000

    created vectors.cpp

---
+++ challenges/3_vectors.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector> //included in C++ Standard Template Library
+using namespace std;
+
+//Operations w/ Vectors---------------
+// iterator begin();
+// iterator end();
+// bool empty();
+// void push_back(const T& x);
+// iterator erase(iterator it);
+// iterator erase(iterator first, iterator last);
+// void clear();
+// Size size();
+
+int main(){ //Testing out Vectors
+    vector <int> intV; // vector of integers
+    vector <string> strV; //vector of strings
+    vector <int>* intPtrV; // vector of pointers to integers
+    
+
+
+}
